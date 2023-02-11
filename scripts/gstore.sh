bd762bd4064a87a678d99f6ffa7a848e778a2815
think <tranthinh.own@gmail.com>
2023-02-11 17:08:07 +0700

    build(core): config structure

---
+++ scripts/gstore.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+NAME=$1
+
+FILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../src" && pwd)
+
+re="[[:space:]]+"
+
+if [ "$#" -ne 1 ] || [[ $NAME =~ $re ]] || [ "$NAME" == "" ]; then
+  echo "Usage: pnpm gc \${name} with no space"
+  exit 1
+fi
+
+DIRNAME="$FILE_PATH/stores/$NAME"
+INPUT_NAME=$NAME
+
+if [ -d "$DIRNAME" ]; then
+  echo "$NAME store already exists, please change it"
+  exit 1
+fi
+
+NORMALIZED_NAME=""
+for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
+  C=$(echo "${i:0:1}" | tr "[:lower:]" "[:upper:]")
+  NORMALIZED_NAME="$NORMALIZED_NAME${C}${i:1}"
+done
+NAME=$NORMALIZED_NAME
+STORE=use${NAME^}Store
+
+touch "$NAME.ts"
+
+cat > $NAME.ts <<EOF
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export $STORE = defineStore('$NAME', () => {
+  return { }
+})
+EOF
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import VueMacros from 'unplugin-vue-macros/dist/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    VueMacros({
+      setupComponent: false,
+      setupSFC: false,
+      plugins: {
+        vue: Vue(),
+        vueJsx: VueJsx(),
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@HRM': fileURLToPath(new URL('./src', import.meta.url)),
