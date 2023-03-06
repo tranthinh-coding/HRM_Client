2971fa8a3215e943f8323a894dcdb8544166a52e
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-06 17:48:43 +0700

    feat: user type

---
+++ src/types/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id?: number
+  name?: string
+  user_id?: string
+  email?: string
+  join_date?: string
+  phone_number?: string
+  status?: string
+  role_name?: string
+  avatar?: string
+  position?: string
+  department?: string
+  email_verified_at?: string
+  created_at?: string
+  updated_at?: string
+  token?: string
+}
