b6dc149044f52af01d1b4f788efe20cb4cac252e
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-06 17:38:22 +0700

    feat(core): pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,91 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-// @ts-ignore
 import VueMacros from 'unplugin-vue-macros/vite'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import path from 'path'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico'],
+  manifest: {
+    name: 'HR Manager',
+    short_name: 'HR Manager',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'img/icons/android-chrome-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/img/icons/android-chrome-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'img/icons/android-chrome-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true,
+  },
+  devOptions: {
+    enabled: true, // process.env.SW_DEV === 'true'
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './dist/hrm',
+  },
   plugins: [
     VueMacros({
-      setupComponent: false,
-      setupSFC: false,
+      setupBlock: true,
       plugins: {
-        vue: Vue(),
+        vue: Vue({
+          include: [/\.vue$/, /\.setup\.[cm]?[jt]sx?$/],
+        }),
         vueJsx: VueJsx(),
       },
     }),
+    vueI18n({
+      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+      // compositionOnly: false,
+
+      // you need to set i18n resource including paths !
+      include: path.resolve(__dirname, './src/lang/**'),
+    }),
+    VitePWA(pwaOptions),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': path.resolve(__dirname, './src'),
     },
   },
 })
