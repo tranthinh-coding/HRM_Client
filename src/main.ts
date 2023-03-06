7d0b9899886867220188880dec120b469a62265d
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-06 17:52:00 +0700

    feat: element-plus + vuesax-alpha

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,38 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import Vuesax from 'vuesax-alpha'
+import 'vuesax-alpha/theme-chalk/index.css'
+
+import i18n from '~/plugins/i18n'
+/**
+ * Service Worker https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps
+ */
+import './registerServiceWorker.ts'
+
+/** Boxicons */
+import '~/assets/boxicons-2.1.2/css/boxicons.min.css'
+
+import '~/styles/index.scss'
+
+// nprogress style
+import 'nprogress/nprogress.css'
 
 const app = createApp(App)
 
+/**
+ * Register Element Plus Icon
+ */
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.use(createPinia())
+
 app.use(router)
+app.use(i18n)
+app.use(Vuesax)
 
 app.mount('#app')
