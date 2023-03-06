f3b2ca1fed235b9831ba3137e24ffa29c36b2096
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-06 17:42:55 +0700

    feat: app config

---
+++ src/config/app.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = 'HR Managerment'
+
+export const ACCESS_TOKEN = 'token'
+export const REMEMBER_TOKEN = 'remember_token'
+
+export const API_V1 = import.meta.env.VITE_APP_API_V1
+export const REGISTER_API = '/register'
+export const LOGIN_API = '/login'
+export const LOGOUT_API = '/logout'
+export const USER_INFO = '/profile'
+export const USER_UPDATE = '/profile'
---
+++ src/config/role.ts
@@ -0,0 +1,4 @@
+export const SUPER_ADMIN = 0
+export const EMPLOYEE = 1
+export const HR = 2
+export const ADMIN = 3
