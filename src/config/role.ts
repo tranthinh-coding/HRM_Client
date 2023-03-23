a55ea0afca8e8fef925fe7af2efc58e25204afb6
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-23 19:32:34 +0700

    feat(config): [role] app role

--- src/config/role.ts
+++ src/config/role.ts
@@ -1,4 +1,7 @@
-export const SUPER_ADMIN = 0
-export const EMPLOYEE = 1
-export const HR = 2
-export const ADMIN = 3
+export const EMPLOYEE = 'Employee'
+export const HR = 'HR Manager'
+export const GUEST = 'Guest'
+
+export const isEmployee = (role: string) => role === EMPLOYEE
+export const isHR = (role: string) => role === HR
+export const isGuest = (role: string) => role === GUEST
