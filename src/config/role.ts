f156bd9af57a475ef67c2ee1e24c237b20d0ee39
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-13 19:11:04 +0700

    refactor: optional param check role

--- src/config/role.ts
+++ src/config/role.ts
@@ -2,6 +2,6 @@ export const EMPLOYEE = 'Employee'
 export const HR = 'HR Manager'
 export const GUEST = 'Guest'
 
-export const isEmployee = (role: string) => role === EMPLOYEE
-export const isHR = (role: string) => role === HR
-export const isGuest = (role: string) => role === GUEST
+export const isEmployee = (role?: string) => role === EMPLOYEE
+export const isHR = (role?: string) => role === HR
+export const isGuest = (role?: string) => role === GUEST
