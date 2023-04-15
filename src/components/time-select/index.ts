2513e232af03bacc2c40f562536987aa7562e81a
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-16 04:07:56 +0700

    chore: component has been exported from vuesax

--- src/components/time-select/index.ts
+++
@@ -1,8 +0,0 @@
-import TimeSelect from './src/time-select.vue'
-import { withInstall } from '~/utils'
-
-export const VsTimeSelect = withInstall(TimeSelect)
-
-export default VsTimeSelect
-
-export * from './src/time-select'
