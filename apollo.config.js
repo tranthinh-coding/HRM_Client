a9e1eb87a70cdbfdd2dc5fcd2721f2310944c35b
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-24 02:20:47 +0700

    fix: apollo service host

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: 'HR Managerment',
       // URL to the GraphQL API
-      url: 'http://hrm_server.test/graphql',
+      url: 'http://localhost/graphql',
     },
     // Files processed by the extension
     includes: ['src/**/*.vue', 'src/**/*.ts'],
