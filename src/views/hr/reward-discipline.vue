d5d87852c574c97481829d41be74d1b1b19afeb2
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-26 02:43:22 +0700

    stagging: reward form

--- src/views/hr/reward-discipline.vue
+++ src/views/hr/reward-discipline.vue
@@ -1,29 +1,55 @@
 <template>
-  <div class="box rounded-2xl p-6">
-    <el-tabs>
-      <el-tab-pane :label="t('page.sidebar.reward')">
-        <div class="flex items-center justify-between">
-          <div>
-            <h3 class="text-xl">Danh sách khen thưởng tháng này</h3>
+  <div>
+    <div class="box rounded-2xl p-6">
+      <el-tabs>
+        <el-tab-pane :label="t('page.sidebar.reward')">
+          <div class="flex items-center justify-between">
+            <div>
+              <h3 class="text-xl">Danh sách khen thưởng tháng này</h3>
+            </div>
+            <div>
+              <vs-button type="shadow" @click="openCreateRewardForm">Thêm</vs-button>
+            </div>
           </div>
-          <div>
-            <vs-button type="shadow">Thêm</vs-button>
-          </div>
-        </div>
-        <el-table :data="[]"></el-table>
-      </el-tab-pane>
+          <el-table :data="[]"></el-table>
+        </el-tab-pane>
+        <el-tab-pane :label="t('page.sidebar.discipline')">
+          Danh sach ky luat
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <vs-dialog v-model="isOpenCreateReward">
+      <template #header>
+        <h3 class="text-lg">Tao khoan khen thuong cho nhan vien</h3>
+      </template>
 
-      <el-tab-pane :label="t('page.sidebar.discipline')">
-        Danh sach ky luat
-      </el-tab-pane>
-    </el-tabs>
+      <vs-input v-model="rewardCreateForm.name" label="Loai khen thuong" label-float />
+      <vs-input v-model="rewardCreateForm.description" label="Mo ta" label-float />
+      <vs-input v-model="rewardCreateForm.salary" label="Muc luong" label-float />
+      <el-date-picker v-model="rewardCreateForm.name" label="Ngay bat dau" />
+    </vs-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
+
+const isOpenCreateReward = ref(false)
+
+const rewardCreateForm = reactive({
+  name: '',
+  description: '',
+  salary: '',
+  start_date: '',
+  user_id: ''
+})
+
+const openCreateRewardForm = () => {
+  isOpenCreateReward.value = true
+}
 </script>
 
 <style scoped lang="scss">
