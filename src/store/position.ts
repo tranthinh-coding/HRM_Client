d41edef0b4c99dc9adfd036c83f4a5a68ef42056
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-26 20:36:28 +0700

    feat: position store

---
+++ src/store/position.ts
@@ -0,0 +1,26 @@
+import { computed } from 'vue'
+import { defineStore } from 'pinia'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+import type { JobPosition } from '~/types'
+
+type QueryResponse = {
+  positions: JobPosition[]
+}
+
+export const usePositionStore = defineStore('Position', () => {
+  const { result, refetch } = useQuery<QueryResponse>(
+    gql`
+      query positions {
+        positions {
+          name
+        }
+      }
+    `
+  )
+
+  const positions = computed(() => result.value?.positions || [])
+
+  return { positions, refetch }
+})
