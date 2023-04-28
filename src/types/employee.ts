f082cf59a8751a70979cfe7b21aab9cef870ee12
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-29 00:54:21 +0700

    type: Employee reward type

--- src/types/employee.ts
+++ src/types/employee.ts
@@ -1,7 +1,21 @@
+import { User } from './user'
+
 export interface Employee {
   name: string
   email: string
   employee_id: string
   position: string
   department: string
+}
+
+export type EmployeeReward = {
+  id: number
+  name: string
+  description: string
+  salary: number
+  start_date: string
+  user_id: string
+  created_at: string
+
+  user: User
 }
