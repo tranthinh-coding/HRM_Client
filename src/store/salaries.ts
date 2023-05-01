a0ee6fef83f942d6b7a5a000f716e9bd37bf26de
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-02 04:17:03 +0700

    thay doi store query

--- src/store/salaries.ts
+++ src/store/salaries.ts
@@ -1,14 +1,17 @@
+import { SalaryPeriod } from './../types/salary'
 import { computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
-import { clone, isNil } from 'lodash-unified'
 
-import { useUserStore } from './user'
-import type { DateFormat, Salary } from '~/types'
+// import { useUserStore } from './user'
+import type { DateFormat } from '~/types'
+import { cloneDeep } from 'lodash-unified'
 
 type QueryResponse = {
-  employee_salaries: Salary[]
+  // employee_salaries: Salary[]
+
+  salary_period: SalaryPeriod[]
 }
 
 type QueryParams = {
@@ -21,7 +24,7 @@ type QueryParams = {
 }
 
 export const useSalariesStore = defineStore('E_SALARY', () => {
-  const user = useUserStore()
+  // const user = useUserStore()
 
   const {
     result,
@@ -29,69 +32,78 @@ export const useSalariesStore = defineStore('E_SALARY', () => {
     stop,
     start,
     restart,
-  } = useQuery<QueryResponse, QueryParams>(
+  } = useQuery<QueryResponse>(
     gql`
-      query EMPLOYEE_SALARIES($user_id: String!) {
-        employee_salaries(user_id: $user_id) {
+      query EMPLOYEE_SALARIES {
+        salary_period {
           id
-          position
-          coefficient_position
-          total_amount
-          salary_calculation
-          base_salary
-          bonus
-          deduction
-          workings_hours
-          leave_hours
+          name
           start_date
           end_date
-          user_id
 
-          user {
-            name
-            email
+          salaries {
+            id
+            position
+            coefficient_position
+            department
+            total_amount
+            base_salary
 
-            employee_information {
-              department
+            bonus
+            deduction
+            workings_hours
+            leave_hours
+
+            start_date
+            end_date
+            user_id
+
+            user {
+              name
+              email
+
+              employee_information {
+                department
+                position
+              }
             }
           }
         }
       }
     `,
-    () => ({
-      user_id: user.user?.user_id || '',
-    }),
+    null,
     {
       pollInterval: 30000,
     }
   )
 
-  const employeeSalaries = computed(() => {
-    const e = result.value?.employee_salaries
-    if (isNil(e)) {
-      return []
-    }
+  const salaryPeriods = computed(() => {
+    const res = cloneDeep(result.value?.salary_period || [])
 
-    return e.reduce((pre, curr) => {
-      const rs = clone(curr)
+    /**
+     * bonus | deduction | working_hours | leave_hours are JSON formatted
+     *
+     * need to parse before returning
+     */
+    res.forEach((e) => {
+      e.salaries.forEach((x) => {
+        x.bonus = JSON.parse(JSON.parse(x.bonus as any))
+        x.deduction = JSON.parse(JSON.parse(x.deduction as any))
+        x.workings_hours = JSON.parse(JSON.parse(x.workings_hours as any))
+        x.leave_hours = JSON.parse(JSON.parse(x.leave_hours as any))
+      })
+    })
 
-      rs.bonus = JSON.parse(JSON.parse(curr.bonus as any))
-      rs.deduction = JSON.parse(JSON.parse(curr.deduction as any))
-      rs.workings_hours = JSON.parse(JSON.parse(curr.workings_hours as any))
-      rs.leave_hours = JSON.parse(JSON.parse(curr.leave_hours as any))
-
-      console.log(rs)
-
-      return [...pre, rs]
-    }, [] as Salary[])
+    console.log(res[0]?.salaries[0].bonus)
+    return res
   })
 
   const refetch = async (query: Omit<QueryParams, 'user_id'>) => {
-    const options = {
-      ...query,
-      user_id: user.user?.user_id || '',
-    }
-    await _refetch(options)
+    // const options = {
+    //   ...query,
+    //   user_id: user.user?.user_id || '',
+    // }
+    await _refetch()
   }
 
   return {
@@ -99,6 +111,6 @@ export const useSalariesStore = defineStore('E_SALARY', () => {
     stop,
     start,
     restart,
-    employeeSalaries,
+    salaryPeriods,
   }
 })
