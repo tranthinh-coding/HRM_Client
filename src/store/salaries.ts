c485cbb5c99232a3c6aace15fd12c15d43cfd134
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-03 18:20:40 +0700

    loai bo QueryParam, the api can only use by HR

--- src/store/salaries.ts
+++ src/store/salaries.ts
@@ -1,38 +1,17 @@
-import { SalaryPeriod } from './../types/salary'
+import { SalaryPeriod } from '~/types'
 import { computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 
-// import { useUserStore } from './user'
-import type { DateFormat } from '~/types'
 import { cloneDeep } from 'lodash-unified'
 
 type QueryResponse = {
-  // employee_salaries: Salary[]
-
   salary_period: SalaryPeriod[]
 }
 
-type QueryParams = {
-  user_id: string
-  salary_calculation?: string
-  department?: string
-  position?: string
-  start_date?: DateFormat
-  end_date?: DateFormat
-}
-
 export const useSalariesStore = defineStore('E_SALARY', () => {
-  // const user = useUserStore()
-
-  const {
-    result,
-    refetch: _refetch,
-    stop,
-    start,
-    restart,
-  } = useQuery<QueryResponse>(
+  const { result, refetch, stop, start, restart } = useQuery<QueryResponse>(
     gql`
       query EMPLOYEE_SALARIES {
         salary_period {
@@ -49,6 +28,7 @@ export const useSalariesStore = defineStore('E_SALARY', () => {
             total_amount
             base_salary
 
+            reward
             bonus
             deduction
             workings_hours
@@ -93,24 +73,15 @@ export const useSalariesStore = defineStore('E_SALARY', () => {
      */
     res.forEach((e) => {
       e.salaries.forEach((x) => {
+        x.reward = JSON.parse(JSON.parse(x.reward as any))
         x.bonus = JSON.parse(JSON.parse(x.bonus as any))
         x.deduction = JSON.parse(JSON.parse(x.deduction as any))
         x.workings_hours = JSON.parse(JSON.parse(x.workings_hours as any))
         x.leave_hours = JSON.parse(JSON.parse(x.leave_hours as any))
       })
     })
-
-    console.log(res[0]?.salaries[0].deduction)
     return res
   })
-
-  const refetch = async (query: Omit<QueryParams, 'user_id'>) => {
-    // const options = {
-    //   ...query,
-    //   user_id: user.user?.user_id || '',
-    // }
-    await _refetch()
-  }
 
   return {
     refetch,
