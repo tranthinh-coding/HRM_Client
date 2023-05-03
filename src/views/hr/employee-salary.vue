3c19cec50e15ba2e42fb8dd65d2b2af71f585334
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-03 23:13:51 +0700

    expect one root element

--- src/views/hr/employee-salary.vue
+++ src/views/hr/employee-salary.vue
@@ -1,47 +1,49 @@
 <template>
-  <vs-button type="gradient" color="danger" @click="openForm = true">
-    Tao bang luong
-  </vs-button>
+  <div>
+    <vs-button type="gradient" color="danger" @click="openForm = true">
+      Tao bang luong
+    </vs-button>
 
-  <div class="box p20px rounded-xl my-20px">
-    <el-table :data="filterTableSalaryPeriods" lazy>
-      <el-table-column label="#" prop="id" />
-      <el-table-column label="Ky tinh luong" prop="name" />
+    <div class="box p20px rounded-xl my-20px">
+      <el-table :data="filterTableSalaryPeriods" lazy>
+        <el-table-column label="#" prop="id" />
+        <el-table-column label="Ky tinh luong" prop="name" />
 
-      <el-table-column
-        width="200"
-        :label="t('page.action')"
-        class-name="table-operations"
-      >
-        <template #header>
-          <vs-input v-model="search" :placeholder="t('page.search')">
-            <template #icon>
-              <el-icon><search-normal /></el-icon>
-            </template>
-          </vs-input>
+        <el-table-column
+          width="200"
+          :label="t('page.action')"
+          class-name="table-operations"
+        >
+          <template #header>
+            <vs-input v-model="search" :placeholder="t('page.search')">
+              <template #icon>
+                <el-icon><search-normal /></el-icon>
+              </template>
+            </vs-input>
+          </template>
+
+          <template #default="{ row }: { row: SalaryPeriod }">
+            <div class="flex items-center justify-center">
+              <vs-button
+                @click="deleteSalaryPeriod(row)"
+                type="transparent"
+                icon
+                color="dribbble"
+              >
+                <el-icon size="20"> <trash /> </el-icon>
+              </vs-button>
+              <vs-button @click="seeDetail(row)" type="transparent" icon>
+                <el-icon size="20"> <info-circle /> </el-icon>
+              </vs-button>
+            </div>
+          </template>
+        </el-table-column>
+
+        <template #empty>
+          <el-empty />
         </template>
-
-        <template #default="{ row }: { row: SalaryPeriod }">
-          <div class="flex items-center justify-center">
-            <vs-button
-              @click="deleteSalaryPeriod(row)"
-              type="transparent"
-              icon
-              color="dribbble"
-            >
-              <el-icon size="20"> <trash /> </el-icon>
-            </vs-button>
-            <vs-button @click="seeDetail(row)" type="transparent" icon>
-              <el-icon size="20"> <info-circle /> </el-icon>
-            </vs-button>
-          </div>
-        </template>
-      </el-table-column>
-
-      <template #empty>
-        <el-empty />
-      </template>
-    </el-table>
+      </el-table>
+    </div>
   </div>
 
   <vs-dialog v-model="openForm">
