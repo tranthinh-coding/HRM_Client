c09e910a97be0527523d49e0f904f2a874d2bb6d
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-07 02:52:00 +0700

    change type of the ErrorResponse

--- src/types/request.ts
+++ src/types/request.ts
@@ -2,7 +2,7 @@ export interface Response {}
 
 export interface ErrorResponse<T extends Object> {
   message: string
-  errors?: Partial<Record<keyof T, string[]>>
+  errors?: T[]
 }
 
 export interface SuccessResponse {
